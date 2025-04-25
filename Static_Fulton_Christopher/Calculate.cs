75c8c09c050a0581b2baad28277657ba5862b46e
Yung-Modi <chrisreaper404@gmail.com>
2025-04-25 13:47:25 -0500

    Initial commit

---
+++ Static_Fulton_Christopher/Calculate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Fulton_Christopher
+{
+    internal class Calculate
+    {
+    }
+}
---
+++ Static_Fulton_Christopher/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Static_Fulton_Christopher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
