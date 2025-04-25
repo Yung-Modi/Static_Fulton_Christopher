c046bbcb2dba42e4e39d0bbf64ac0b98e0f96c9c
Yung-Modi <chrisreaper404@gmail.com>
2025-04-25 13:48:03 -0500

    g

--- Static_Fulton_Christopher/Calculate.cs
+++ Static_Fulton_Christopher/Calculate.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Static_Fulton_Christopher
 {
-    internal class Calculate
+    static class Calculate
     {
+
+        public static float result = 0.0f;
+
+        static Calculate()
+        {
+            result = 0.0f;
+
+        }
+
+        // integers
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+        // floaters
+
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+
+        public static float Sub(float x, float y)
+        {
+            return x - y;
+        }
+
+        public static float Mult(float x, float y)
+        {
+            return x * y;
+        }
+
+        public static float Div(float x, float y)
+        {
+            return x / y;
+        }
+
     }
 }
--- Static_Fulton_Christopher/Program.cs
+++ Static_Fulton_Christopher/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Static_Fulton_Christopher
+﻿using System;
+
+namespace Static_Fulton_Christopher
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Integers
+            Console.WriteLine("Integer Methods");
+            Console.WriteLine("15 + 88 is " + Calculate.Add(15, 88));
+            Console.WriteLine(Calculate.Sub(78, 88));
+            Console.WriteLine(Calculate.Mult(8, 9));
+            Console.WriteLine(Calculate.Div(77, 8));
+
+            // Floats
+            Console.WriteLine("\nFloat Mehtods");
+            Console.WriteLine(Calculate.Add(15.55f, 88.65f));
+            Console.WriteLine(Calculate.Sub(78.45f, 88.89f));
+            Console.WriteLine(Calculate.Mult(8.99f, 9.65f));
+            Console.WriteLine(Calculate.Div(77.265f, 8.0f));
         }
     }
 }
